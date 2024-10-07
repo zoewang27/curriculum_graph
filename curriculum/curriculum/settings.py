d0dd73c92c379239fc249cd4d04705361df3ccae
zoewang27 <zmwang27@gmail.com>
2024-10-07 16:26:01 +0200

    delete env

--- curriculum/curriculum/settings.py
+++ curriculum/curriculum/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import environ
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
