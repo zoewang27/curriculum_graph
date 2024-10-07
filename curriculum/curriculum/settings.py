54b233fc7afb67fc9a1fa63a5f22c6e2d7b4a9ca
zoewang27 <zmwang27@gmail.com>
2024-10-07 17:09:20 +0200

    update setting.py

--- curriculum/curriculum/settings.py
+++ curriculum/curriculum/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-+*l8vnf9h7j@8d48r2)7x_pge2*zc2sl+y(#+y-)yaf5w=$wam
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['145.100.135.3', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['145.100.135.3', 'localhost', '127.0.0.1','ckg.lab.uvalight.net']
+
+CSRF_TRUSTED_ORIGINS = ['https://ckg.lab.uvalight.net']
 
 # Application definition
 
